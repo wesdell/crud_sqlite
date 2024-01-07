9d55d0cb06232ed8cf81576b60c0e5a496c65661
Wesdell <wesdell17@gmail.com>
2024-01-07 09:54:56 -0500

    Created class to get SQLite connection

---
+++ crud_sqlite/database/Connection.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.SQLite;
+
+namespace crud_sqlite.database
+{
+    public class Connection
+    {
+        private string _connectionString;
+        private static Connection _connection = null;
+
+        private Connection()
+        {
+            this._connectionString = "./database.db";
+        }
+
+        public SQLiteConnection Connect()
+        {
+            SQLiteConnection connection = new SQLiteConnection();
+
+            try
+            {
+                connection.ConnectionString = "Data Source=" + this._connectionString;
+            }
+            catch (Exception error)
+            {
+                connection = null;
+                throw error;
+            }
+            return connection;
+        }
+
+        public static Connection getConnection()
+        {
+            if (_connection == null)
+            {
+                _connection = new Connection();
+            }
+            return _connection;
+        }
+    }
+}
