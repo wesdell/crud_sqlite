2f0c153af821db9e552639970e9555524f0b803a
Wesdell <wesdell17@gmail.com>
2024-02-03 16:17:08 -0500

    Item model properties

---
+++ crud_sqlite/models/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crud_sqlite.models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+
+        public int Measure_Id { get; set; }
+
+        public int Category_Id { get; set; }
+
+        public string Description { get; set; }
+
+        public string Brand { get; set; }
+    }
+}
