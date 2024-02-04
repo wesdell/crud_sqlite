9caa3b72070486ffb508cb193654a2ff80d166c0
Wesdell <wesdell17@gmail.com>
2024-02-04 16:01:58 -0500

    Format data grid service

---
+++ crud_sqlite/services/Format.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace crud_sqlite.services
+{
+    public class Format
+    {
+        public void FormatDataGrid(DataGrid dataGrid)
+        {
+            for (int i = 0; i < dataGrid.Columns.Count; i += 1)
+            {
+                dataGrid.Columns[i].Width = 90;
+            }
+        }
+    }
+}
