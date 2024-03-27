5a10136207dd9c8dc374528f34c8ef294e2112d9
Wesdell <wesdell17@gmail.com>
2024-03-27 14:25:34 -0500

    Endpoint to get categories from db

---
+++ crud_sqlite/database/Categories.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crud_sqlite.database
+{
+    public class Categories
+    {
+        public DataTable GetCategories()
+        {
+            SQLiteDataReader categories;
+            DataTable dataTable = new DataTable();
+            SQLiteConnection SQLiteConnection = new SQLiteConnection();
+
+            try
+            {
+                SQLiteConnection = Connection.getConnection().Connect();
+
+                String SQLQuery = "SELECT id, description FROM category;";
+                SQLiteCommand SQLCommand = new SQLiteCommand(SQLQuery, SQLiteConnection);
+                SQLiteConnection.Open();
+
+                categories = SQLCommand.ExecuteReader();
+                dataTable.Load(categories);
+
+                return dataTable;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (SQLiteConnection.State == ConnectionState.Open)
+                {
+                    SQLiteConnection.Close();
+                }
+            }
+        }
+
+        public String GetCategoryById(int categoryId)
+        {
+            String categoryDescription;
+            SQLiteConnection SQLiteConnection = new SQLiteConnection();
+
+            try
+            {
+                SQLiteConnection = Connection.getConnection().Connect();
+
+                String SQLQuery = "SELECT description FROM category WHERE id = @CategoryId";
+                SQLiteCommand SQLCommand = new SQLiteCommand(SQLQuery, SQLiteConnection);
+                SQLCommand.Parameters.AddWithValue("@CategoryId", categoryId);
+                SQLiteConnection.Open();
+
+                categoryDescription = SQLCommand.ExecuteScalar().ToString();
+                return categoryDescription;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (SQLiteConnection.State == ConnectionState.Open)
+                {
+                    SQLiteConnection.Close();
+                }
+            }
+        }
+    }
+}
