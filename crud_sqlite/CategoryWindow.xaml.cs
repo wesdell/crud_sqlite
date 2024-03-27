3c555aa159197b8ec663309970a393d07a91e5fb
Wesdell <wesdell17@gmail.com>
2024-03-27 14:26:18 -0500

    Category window was added

---
+++ crud_sqlite/CategoryWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using crud_sqlite.database;
+using crud_sqlite.services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace crud_sqlite
+{
+    public partial class CategoryWindow : Window
+    {
+        public CategoryWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void FormatCategoryDataTable()
+        {
+            Format format = new Format();
+            format.FormatDataGrid(dataGridCategory);
+        }
+
+        private void GetCategories()
+        {
+            Categories categories = new Categories();
+            dataGridCategory.ItemsSource = categories.GetCategories().DefaultView;
+            dataGridCategory.SelectedValuePath = "id";
+            this.FormatCategoryDataTable();
+        }
+
+        private void ApplyCategory(object sender, RoutedEventArgs e)
+        {
+            if (dataGridCategory.SelectedValue != null)
+            {
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Select a register.", "System message", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.GetCategories();
+        }
+    }
+}
